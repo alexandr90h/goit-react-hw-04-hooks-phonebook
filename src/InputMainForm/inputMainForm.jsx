bc62dbbe78dae747cc007412a655ba721f2ca735
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-12-18 16:01:51 +0200

    18.12.2020-1

--- src/InputMainForm/inputMainForm.jsx
+++ src/InputMainForm/inputMainForm.jsx
@@ -1,42 +1,86 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import styles from './inputMainForm.module.scss';
 import { v4 as uuidv4 } from 'uuid';
-class InputForm extends Component{
-    state={
-        name: '',
-        number: '',
-        id: uuidv4(),
+import { useState } from 'react';
+
+export default function InputMainForm({name,number,id}) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [id, setid] = useState(uuidv4());
+    
+    const hendleInputChanga = e => {
+        switch (e.target.name) {
+            case 'name': setName(e.target.value)
+                break;
+            case 'number': setNumber(e.target.value)
+                break;
+            default:
+                break;
+        }
     }
-    hendleInputChanga = e => {
-        const { name, value } = e.currentTarget;
-        this.setState({
-            [name]: value,
-        })
-    }
-    hendleOnSubmit = e => {
+    const hendleOnSubmit = e => {
         e.preventDefault();
         this.props.onSubHand(this.state);
-        this.reset();
+        reset();
     }
-    reset=() => {
-        this.setState({
-            name: '',
-            number: '',
-            id: uuidv4(),
-        })
+    const reset = () => {
+        setName('');
+        setNumber('');
+        setid(uuidv4());
     }
-    render() {
-        return (<div>
-            <form onSubmit={this.hendleOnSubmit} className={styles.mainForm}>
+     return (
+            <div>
+            <form onSubmit={hendleOnSubmit} className={styles.mainForm}>
                 <label>Name 
-            <input type="text" name="name" autoComplete="off" value={this.state.name} onChange={this.hendleInputChanga}/>
+            <input type="text" name="name" autoComplete="off" value={name} onChange={hendleInputChanga}/>
                 </label>
                 <label>Number
-                     <input type="text" name="number" autoComplete="off" value={this.state.number} onChange={this.hendleInputChanga}/>
+                     <input type="text" name="number" autoComplete="off" value={number} onChange={hendleInputChanga}/>
                 </label>
                 <button type="submit">Add contact</button>
         </form>
-        </div>);
-    }
-}
-export default InputForm; 
+            </div>
+        );
+};
+
+// class InputForm extends Component{
+//     state={
+//         name: '',
+//         number: '',
+//         id: uuidv4(),
+//     }
+//     hendleInputChanga = e => {
+//         const { name, value } = e.currentTarget;
+//         this.setState({
+//             [name]: value,
+//         })
+//     }
+//     hendleOnSubmit = e => {
+//         e.preventDefault();
+//         this.props.onSubHand(this.state);
+//         this.reset();
+//     }
+//     reset=() => {
+//         this.setState({
+//             name: '',
+//             number: '',
+//             id: uuidv4(),
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>
+//             <form onSubmit={this.hendleOnSubmit} className={styles.mainForm}>
+//                 <label>Name 
+//             <input type="text" name="name" autoComplete="off" value={this.state.name} onChange={this.hendleInputChanga}/>
+//                 </label>
+//                 <label>Number
+//                      <input type="text" name="number" autoComplete="off" value={this.state.number} onChange={this.hendleInputChanga}/>
+//                 </label>
+//                 <button type="submit">Add contact</button>
+//         </form>
+//             </div>
+//         );
+//     }
+// }
+// export default InputForm; 
