8b6543c94539a546acf9c4b655ef7b0657d218a9
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2021-01-11 18:31:13 +0200

    11.01.2021-1

--- src/InputMainForm/InputMainForm.jsx
+++ src/InputMainForm/InputMainForm.jsx
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types';
 import styles from './inputMainForm.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function InputMainForm(props) {
+    const { register, hendleOnSubmit, er } = useForm();
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
     const [id, setId] = useState(uuidv4());
@@ -18,7 +20,7 @@ export default function InputMainForm(props) {
                 break;
         }
     }
-    const hendleOnSubmit = e => {
+    const hendleOnSubmite = e => {
         e.preventDefault();
        props.onSubHand({ name, number, id });
         reset();
@@ -30,9 +32,9 @@ export default function InputMainForm(props) {
     }
      return (
             <div>
-            <form onSubmit={hendleOnSubmit} className={styles.mainForm}>
+            <form onSubmit={hendleOnSubmit(hendleOnSubmite)} className={styles.mainForm}>
                 <label>Name 
-            <input type="text" name="name" autoComplete="off" value={name} onChange={hendleInputChanga}/>
+            <input type="text" name="name" autoComplete="off" value={name} onChange={hendleInputChanga} ref={register({required:true,minLength:3})}/>
                 </label>
                 <label>Number
                      <input type="text" name="number" autoComplete="off" value={number} onChange={hendleInputChanga}/>
